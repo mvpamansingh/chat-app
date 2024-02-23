d3ee4bfef620dc136702b096425504a25ffb4473
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-02-23 21:42:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const dotenv= require('dotenv');
+
+
+
+dotenv.config();
+const app = express();
+
+const server= require('http').createServer(app);
+const io=  require('socket.io')(server);
+
+app.get('/', (req,res)=>{
+    res.send('server running at 3000...hu')
+});
+
+
+io.on('connection', (data)=>{
+    console.log(`connection data: ${data}`);
+})
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log(`server is running at  ${port}...`)
+})
